310dd920c7e1b4d62ffc2d5c3c1e74abf352f612
adityaviren <adityaviren@gmail.com>
2020-09-28 17:38:17 +0530

    added daily wage

--- src/employeeWageComputation/EmployeeWageComputation.java
+++ src/employeeWageComputation/EmployeeWageComputation.java
@@ -3,7 +3,8 @@ package employeeWageComputation;
 import java.util.Random;
 
 public class EmployeeWageComputation {
-	
+	private static final int wage_per_hour = 20;
+	private static final int full_hours = 8;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to employee wage computation programme");
@@ -13,6 +14,8 @@ public class EmployeeWageComputation {
 			System.out.println("Employee Absent");
 		else
 			System.out.println("Employee Present");
+		int daily_wage=wage_per_hour*full_hours*is_present;
+		System.out.println("Daily wage is "+daily_wage);
 	}
 
 }
