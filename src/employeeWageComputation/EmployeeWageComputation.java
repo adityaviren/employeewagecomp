67d354a6e0e998695fd4c99f447fdf188663aba9
adityaviren <adityaviren@gmail.com>
2020-09-28 17:31:50 +0530

    Added random in main method

--- src/employeeWageComputation/EmployeeWageComputation.java
+++ src/employeeWageComputation/EmployeeWageComputation.java
@@ -1,10 +1,18 @@
 package employeeWageComputation;
 
-public class EmployeeWageComputation {
+import java.util.Random;
 
+public class EmployeeWageComputation {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to employee wage computation programme");
+		Random random = new Random();
+		int is_present = (int) random.nextInt(2);
+		if(is_present==0)
+			System.out.println("Employee Absent");
+		else
+			System.out.println("Employee Present");
 	}
 
 }
