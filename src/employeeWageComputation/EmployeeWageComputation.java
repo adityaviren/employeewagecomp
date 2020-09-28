9354c3279e9e5a9ce02d21f09a23ee029696ade1
adityaviren <adityaviren@gmail.com>
2020-09-28 17:48:13 +0530

    added part time

--- src/employeeWageComputation/EmployeeWageComputation.java
+++ src/employeeWageComputation/EmployeeWageComputation.java
@@ -5,16 +5,25 @@ import java.util.Random;
 public class EmployeeWageComputation {
 	private static final int wage_per_hour = 20;
 	private static final int full_hours = 8;
+	private static final int part_hours = 8;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to employee wage computation programme");
+		int daily_wage=0;
 		Random random = new Random();
-		int is_present = (int) random.nextInt(2);
-		if(is_present==0)
+		int is_present = (int) random.nextInt(3);
+		if(is_present==0) {
 			System.out.println("Employee Absent");
-		else
-			System.out.println("Employee Present");
-		int daily_wage=wage_per_hour*full_hours*is_present;
+			daily_wage=wage_per_hour*0;
+		}
+		else if(is_present==1) {
+			System.out.println("Part Time");
+			daily_wage=wage_per_hour*part_hours;
+		}
+		else {
+			System.out.println("Full time");
+			daily_wage=wage_per_hour*full_hours;
+		}
 		System.out.println("Daily wage is "+daily_wage);
 	}
 
