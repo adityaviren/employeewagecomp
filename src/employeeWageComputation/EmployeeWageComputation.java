3a86c2e4cb48bff7880fbeccaf49edb18ab0d40c
adityaviren <adityaviren@gmail.com>
2020-09-28 17:53:20 +0530

    modified if else to switch

--- src/employeeWageComputation/EmployeeWageComputation.java
+++ src/employeeWageComputation/EmployeeWageComputation.java
@@ -12,17 +12,19 @@ public class EmployeeWageComputation {
 		int daily_wage=0;
 		Random random = new Random();
 		int is_present = (int) random.nextInt(3);
-		if(is_present==0) {
+		switch(is_present) {
+		case 0:
 			System.out.println("Employee Absent");
 			daily_wage=wage_per_hour*0;
-		}
-		else if(is_present==1) {
+			break;
+		case 1:
 			System.out.println("Part Time");
 			daily_wage=wage_per_hour*part_hours;
-		}
-		else {
+			break;
+		case 2:
 			System.out.println("Full time");
 			daily_wage=wage_per_hour*full_hours;
+			break;
 		}
 		System.out.println("Daily wage is "+daily_wage);
 	}
